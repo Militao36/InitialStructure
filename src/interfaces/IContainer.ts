8bbfa790fca4ce465fa017ba877a219d9cc84815
Militao36 <matheusmoreira3693@gmail.com>
2021-09-22 17:11:38 -0300

    Adicionado novas funcionalidades e também comando ace

--- src/interfaces/IContainer.ts
+++ src/interfaces/IContainer.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { UserRepo } from '../repositories/User'
 
 export default interface IContainer {
     prisma: PrismaClient,
-    userRepo: UserRepo
 }
