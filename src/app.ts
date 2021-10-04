ef6aa39eab22b1a6574af8617b97f693b1e4cd23
Militao36 <matheusmoreira3693@gmail.com>
2021-10-04 20:55:48 -0300

    Adicionado middleware para tratar error

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import { loadControllers, scopePerRequest } from 'awilix-express'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import container from './container'
 
 const app = express()
 
 app.use(express.json())
 
-app.use((request, response, next) => {
+app.use((request, _, next) => {
   if (Object.keys(request.body).length) {
     Object.keys(request.body).forEach((key) => {
       request.body[key] = request.body[key] !== '' ? request.body[key] : null
@@ -20,10 +20,12 @@ app.use(scopePerRequest(container))
 
 app.use(loadControllers('controllers/*.ts', { cwd: __dirname }))
 
-app.get('/version', (req, res) => {
-  return res
-    .status(200)
-    .send('1.0.0')
+app.get('/version', (req, res) => res.status(200).send('1.0.0'))
+
+app.use((err: any, _: Request, res: Response, next: NextFunction) => {
+  return res.status(err.statusCode || 500).json({
+    error: err.message
+  })
 })
 
 export default app
