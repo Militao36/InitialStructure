b0f30386ed43cd8015735e58a8824461cc8545df
Militao36 <matheusmoreira3693@gmail.com>
2021-11-29 11:03:25 -0300

    Commit

--- src/interfaces/IBaseService.ts
+++
@@ -1,7 +0,0 @@
-export interface IBaseService{
-  save: (data: any) => Promise<void>
-  update (data: any, id: string): Promise<void>
-  findById (id: string): Promise<any>
-  findByIdOrFail (id: string): Promise<any>
-  delete (id: string): Promise<void>
-}
